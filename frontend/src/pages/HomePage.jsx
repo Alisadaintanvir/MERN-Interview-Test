6dce1dc84ba90fccbab5a4f38d122ca8c7ea2956
alisadaintanvir <alisadaintanvir@gmail.com>
2024-09-15 02:44:36 +0600

    add drawlist page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
 function HomePage() {
   return (
-    <div>
-      <h1>This is a Homepage</h1>
-    </div>
+    <main>
+      <div className="container mx-auto py-20 space-y-14">
+        <div className="title">
+          <h2 className="text-4xl font-semibold">Drawing List</h2>
+        </div>
+
+        <div className="list grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+          <div className="card h-48 border shadow-md"></div>
+        </div>
+      </div>
+    </main>
   );
 }
 
